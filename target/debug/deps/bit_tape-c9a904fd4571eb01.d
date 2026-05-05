9ccab35e12a6eef38b1cf74b0a03097cb49e1494
FallBackITA27 <diegomassanova4@gmail.com>
2026-05-05 11:41:52 +0200

    add gitignore

--- target/debug/deps/bit_tape-c9a904fd4571eb01.d
+++
@@ -1,7 +0,0 @@
-/home/falb/bit_tape/target/debug/deps/bit_tape-c9a904fd4571eb01.d: src/lib.rs
-
-/home/falb/bit_tape/target/debug/deps/libbit_tape-c9a904fd4571eb01.rlib: src/lib.rs
-
-/home/falb/bit_tape/target/debug/deps/libbit_tape-c9a904fd4571eb01.rmeta: src/lib.rs
-
-src/lib.rs:
--- target/debug/libbit_tape.d
+++
@@ -1,1 +0,0 @@
-/home/falb/bit_tape/target/debug/libbit_tape.rlib: /home/falb/bit_tape/target/package/bit_tape-0.1.0/src/lib.rs
